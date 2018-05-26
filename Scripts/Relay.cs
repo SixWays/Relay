478ebe86beb603c75fdaa123cd1e243cf599c386
Tim Aksu <timur.s.aksu@gmail.com>
2018-05-26 13:26:28 +1000

    Created RelayDelegateCollection to replace manually managing both persistant and once arrays of listeners
    Less code, less error prone.

--- Scripts/Relay.cs
+++ Scripts/Relay.cs
@@ -39,27 +39,111 @@ using Sigtrap.Relays.Binding;
 
 namespace Sigtrap.Relays {
 	public abstract class RelayBase<TDelegate> : IRelayLinkBase<TDelegate> where TDelegate:class {
-		/// <summary>
-		/// How many persistent listeners does this intance currently have?
-		/// </summary>
-		public uint listenerCount {get {return _count;}}
+
+	    public class RelayDelegateCollection
+	    {
+	        public TDelegate[] listeners { get; private set; }
+	        public uint count { get; private set; }
+	        private uint _cap;
+
+	        public RelayDelegateCollection()
+	        {
+	            listeners = new TDelegate[1];
+	            _cap = 1;
+	        }
+
+	        public bool Add(TDelegate listener, bool allowDuplicates = false)
+	        {
+	            if (!allowDuplicates && Contains(listener)) return false;
+
+	            if (RequiresExpansion())
+	                Expand();
+
+	            listeners[count] = listener;
+	            ++count;
+
+	            return true;
+            }
+
+	        public bool Contains(TDelegate listener)
+	        {
+	            for (uint i = 0; i < count; ++i)
+	            {
+	                if (listeners[i].Equals(listener))
+	                {
+	                    return true;
+	                }
+	            }
+	            return false;
+            }
+
+	        public bool Remove(TDelegate listener)
+	        {
+	            for (uint i = 0; i < count; ++i)
+	            {
+	                if (listeners[i].Equals(listener))
+	                {
+	                    RemoveAt(i);
+	                    return true;
+	                }
+	            }
+
+	            return false;
+	        }
+
+	        public void RemoveAt(uint i)
+	        {
+	            for (uint j = i; j < count; ++j)
+	            {
+	                listeners[j] = listeners[j + 1];
+	            }
+	            listeners[count] = null;
+
+	            count--;
+	        }
+
+	        private bool RequiresExpansion()
+	        {
+	            return count == _cap;
+	        }
+
+            private void Expand()
+	        {
+	            var newCap = _cap * 2;
+
+	            TDelegate[] newArr = new TDelegate[newCap];
+	            for (int i = 0; i < count; ++i)
+	            {
+	                newArr[i] = listeners[i];
+	            }
+
+	            _cap = newCap;
+                listeners = newArr;
+	        }
+
+	        public void Clear()
+	        {
+	            Array.Clear(listeners, 0, (int)_cap);
+	            count = 0;
+            }
+	    }
+
+        /// <summary>
+        /// How many persistent listeners does this intance currently have?
+        /// </summary>
+        public uint listenerCount {get {return persistantListeners.count; }}
 		/// <summary>
 		/// How many one-time listeners does this intance currently have?
 		/// After dispatch, all current one-time listeners are automatically removed.
 		/// </summary>
-		public uint oneTimeListenersCount {get {return _onceCount;}}
+		public uint oneTimeListenersCount {get {return onceListeners.count; } }
 
 		protected bool _hasLink = false;
 
-		protected TDelegate[] _listeners = new TDelegate[1];
-		protected uint _count = 0;
-		protected uint _cap = 1;
+	    protected RelayDelegateCollection persistantListeners = new RelayDelegateCollection();
+	    protected RelayDelegateCollection onceListeners = new RelayDelegateCollection();
 
-		protected TDelegate[] _listenersOnce;
-		protected uint _onceCount = 0;
-		protected uint _onceCap = 0;
-
-		protected static IndexOutOfRangeException _eIOOR = new IndexOutOfRangeException("Fewer listeners than expected. See guidelines in Relay.cs on using RemoveListener and RemoveAll within Relay listeners.");
+        protected static IndexOutOfRangeException _eIOOR = new IndexOutOfRangeException("Fewer listeners than expected. See guidelines in Relay.cs on using RemoveListener and RemoveAll within Relay listeners.");
 
 		#if SIGTRAP_RELAY_DBG
 		/// <summary>
@@ -94,7 +178,7 @@ namespace Sigtrap.Relays {
 		/// </summary>
 		/// <param name="listener">Listener.</param>
 		public bool Contains(TDelegate listener){
-			return Contains(_listeners, _count, listener);
+			return persistantListeners.Contains(listener);
 		}
 		/// <summary>
 		/// Adds a persistent listener.
@@ -103,18 +187,7 @@ namespace Sigtrap.Relays {
 		/// <param name="listener">Listener.</param>
 		/// <param name="allowDuplicates">If <c>false</c>, checks whether persistent listener is already present.</param>
 		public bool AddListener(TDelegate listener, bool allowDuplicates=false){
-			if (!allowDuplicates && Contains(listener)) return false;
-			if (_count == _cap){
-				_cap *= 2;
-				_listeners = Expand(_listeners, _cap, _count);
-			}
-			_listeners[_count] = listener;
-			++_count;
-			#if SIGTRAP_RELAY_DBG
-			_RelayDebugger.DebugAddListener(this, listener);
-			#endif
-
-			return true;
+		    return Add(persistantListeners, listener, allowDuplicates);
 		}
 		/// <summary>
 		/// Adds listener and creates a RelayBinding between the listener and the Relay.
@@ -137,21 +210,7 @@ namespace Sigtrap.Relays {
 		/// <param name="listener">Listener.</param>
 		/// <param name="allowDuplicates">If <c>false</c>, checks whether one-time listener is already present.</param>
 		public bool AddOnce(TDelegate listener, bool allowDuplicates=false){
-			if (!allowDuplicates && Contains(_listenersOnce, _onceCount, listener)) return false;
-			if (_onceCount == _onceCap){
-				if (_onceCap == 0){
-					_onceCap = 1;
-				} else {
-					_onceCap *= 2;
-				}
-				_listenersOnce = Expand(_listenersOnce, _onceCap, _onceCount);
-			}
-			_listenersOnce[_onceCount] = listener;
-			++_onceCount;
-			#if SIGTRAP_RELAY_DBG
-			_RelayDebugger.DebugAddListener(this, listener);
-			#endif
-			return true;
+		    return Add(onceListeners, listener, allowDuplicates);
 		}
 		/// <summary>
 		/// Removes a persistent listener, if present.
@@ -159,18 +218,7 @@ namespace Sigtrap.Relays {
 		/// <returns><c>true</c>, if listener was removed, <c>false</c> otherwise.</returns>
 		/// <param name="listener">Listener.</param>
 		public bool RemoveListener(TDelegate listener){
-			bool result = false;
-			for (uint i=0; i<_count; ++i){
-				if (_listeners[i].Equals(listener)) {
-					RemoveAt(i);
-					result = true;
-					break;
-				}
-			}
-			#if SIGTRAP_RELAY_DBG
-			if (result) _RelayDebugger.DebugRemListener(this, listener);
-			#endif
-			return result;
+		    return Remove(persistantListeners, listener);
 		}
 		/// <summary>
 		/// Removes a one-time listener, if present.
@@ -178,74 +226,65 @@ namespace Sigtrap.Relays {
 		/// <returns><c>true</c>, if listener was removed, <c>false</c> otherwise.</returns>
 		/// <param name="listener">Listener.</param>
 		public bool RemoveOnce(TDelegate listener){
-			bool result = false;
-			for (uint i=0; i<_onceCount; ++i){
-				if (_listenersOnce[i].Equals(listener)) {
-					RemoveOnceAt(i);
-					result = true;
-					break;
-				}
-			}
-			#if SIGTRAP_RELAY_DBG
-			if (result) _RelayDebugger.DebugRemListener(this, listener);
-			#endif
-			return result;
+		    return Remove(onceListeners, listener);
 		}
-		#endregion
+
 		/// <summary>
 		/// Removes all listeners.
 		/// </summary>
 		/// <param name="removePersistentListeners">If set to <c>true</c> remove persistent listeners.</param>
 		/// <param name="removeOneTimeListeners">If set to <c>true</c>, also remove one-time listeners.</param>
 		public void RemoveAll(bool removePersistentListeners=true, bool removeOneTimeListeners=false){
+
 			if (removePersistentListeners) {
 				#if SIGTRAP_RELAY_DBG
-				for (int i=0; i<_listeners.Length; ++i){
-					_RelayDebugger.DebugRemListener(this, _listeners[i]);
+				for (int i=0; i< persistantListeners.count; ++i){
+					_RelayDebugger.DebugRemListener(this, persistantListeners.listeners[i]);
+				}
+                #endif
+
+			    persistantListeners.Clear();
+            }
+
+			if (removeOneTimeListeners){
+                #if SIGTRAP_RELAY_DBG
+				for (int i=0; i< onceListeners.count; ++i){
+					_RelayDebugger.DebugRemListener(this, onceListeners.listeners[i]);
 				}
 				#endif
-				Array.Clear(_listeners, 0, (int)_cap);
-				_count = 0;
-			}
-			if (removeOneTimeListeners){
-				Array.Clear(_listenersOnce, 0, (int)_onceCap);
-				_onceCount = 0;
+
+			    onceListeners.Clear();
 			}
 		}
 
-		#region Internal
-		protected void RemoveAt(uint i){
-			_count = RemoveAt(_listeners, _count, i);
-		}
-		protected void RemoveOnceAt(uint i){
-			_onceCount = RemoveAt(_listenersOnce, _onceCount, i);
-		}
-		protected uint RemoveAt(TDelegate[] arr, uint count, uint i){
-			--count;
-			for (uint j=i; j<count; ++j){
-				arr[j] = arr[j+1];
-			}
-			arr[count] = null;
-			return count;
-		}
+	    #endregion
 
-		bool Contains(TDelegate[] arr, uint c, TDelegate d){
-			for (uint i=0; i<c; ++i){
-				if (arr[i].Equals(d)) {
-					return true;
-				}
-			}
-			return false;
-		}
-		TDelegate[] Expand(TDelegate[] arr, uint cap, uint count){
-			TDelegate[] newArr = new TDelegate[cap];
-			for (int i=0; i<count; ++i){
-				newArr[i] = arr[i];
-			}
-			return newArr;
-		}
-		#endregion
-	}
+        #region Internal
+
+	    private bool Add(RelayDelegateCollection delegateCollection, TDelegate listener, bool allowDuplicates)
+	    {
+	        bool wasAdded = delegateCollection.Add(listener, allowDuplicates);
+
+#if SIGTRAP_RELAY_DBG
+	        if (wasAdded) _RelayDebugger.DebugAddListener(this, listener);
+#endif
+	        return true;
+        }
+
+	    private bool Remove(RelayDelegateCollection delegateCollection, TDelegate listener){
+
+	        bool wasRemoved = delegateCollection.Remove(listener);
+
+#if SIGTRAP_RELAY_DBG
+	        if (wasRemoved) _RelayDebugger.DebugRemListener(this, listener);
+#endif
+
+	        return wasRemoved;
+        }
+
+        #endregion
+
+    }
 
 	#region Implementations
 	public class Relay : RelayBase<Action>, IRelayLink {
@@ -265,32 +304,35 @@ namespace Sigtrap.Relays {
 		}
 
 		public void Dispatch(){
-			// Persistent listeners
-			// Reversal allows self-removal during dispatch (doesn't skip next listener)
-			// Reversal allows safe addition during dispatch (doesn't fire immediately)
-			for (uint i=_count; i>0; --i){		
-				if (i>_count) throw _eIOOR;		
-				if (_listeners[i-1] != null){	
-					_listeners[i-1]();
-				} else {
-					RemoveAt(i-1);
-				}
-			}
-			// One-time listeners - reversed for safe addition and auto-removal
-			for (uint i=_onceCount; i>0; --i){
-				if (_listenersOnce[i-1] != null){
-					var l = _listenersOnce[i-1];
-					l();
-					// Check for self-removal before auto-removing
-					if (_listenersOnce[i-1] == l){
-						#if SIGTRAP_RELAY_DBG
-						_RelayDebugger.DebugRemListener(this, _listenersOnce[i-1]);
-						#endif
-						_onceCount = RemoveAt(_listenersOnce, _onceCount, i-1);
-					}
-				}
-			}
+		    IterateAndInvoke(persistantListeners, false);
+		    IterateAndInvoke(onceListeners, true);
 		}
+
+	    void IterateAndInvoke(RelayDelegateCollection collection, bool removeAfterInvoke){
+	        // Reversal allows self-removal during dispatch (doesn't skip next listener)
+	        // Reversal allows safe addition during dispatch (doesn't fire immediately)
+	        for (uint i = collection.count; i > 0; --i)
+	        {
+	            if (i > collection.count) throw _eIOOR;
+	            if (collection.listeners[i - 1] != null)
+	            {
+	                var l = collection.listeners[i - 1];
+                    l();
+					if (removeAfterInvoke){
+					    // Check for self-removal before auto-removing
+					    if (onceListeners.listeners[i - 1] == l){
+						    #if SIGTRAP_RELAY_DBG
+						        _RelayDebugger.DebugRemListener(this, onceListeners.listeners[i-1]);
+                            #endif
+					        onceListeners.RemoveAt(i-1);
+					    }
+					}
+	            }
+	            else {
+	                collection.RemoveAt(i - 1);
+	            }
+	        }
+        }
 	}
 	public class Relay<T> : RelayBase<Action<T>>, IRelayLink<T> {
 		private IRelayLink<T> _link = null;
@@ -309,27 +351,32 @@ namespace Sigtrap.Relays {
 		}
 
 		public void Dispatch(T t){
-			for (uint i=_count; i>0; --i){
-				if (i>_count) throw _eIOOR;
-				if (_listeners[i-1] != null){
-					_listeners[i-1](t);
-				} else {
-					RemoveAt(i-1);
-				}
-			}
-			for (uint i=_onceCount; i>0; --i){
-				if (_listenersOnce[i-1] != null){
-					var l = _listenersOnce[i-1];
-					l(t);
-					if (_listenersOnce[i-1] == l){
-						#if SIGTRAP_RELAY_DBG
-						_RelayDebugger.DebugRemListener(this, _listenersOnce[i-1]);
-						#endif
-						_onceCount = RemoveAt(_listenersOnce, _onceCount, i-1);
-					}
-				}
-			}
+		    IterateAndInvoke(persistantListeners, false, t);
+		    IterateAndInvoke(onceListeners, true, t);
 		}
+
+	    void IterateAndInvoke(RelayDelegateCollection collection, bool removeAfterInvoke, T t){
+	        for (uint i = collection.count; i > 0; --i)
+	        {
+	            if (i > collection.count) throw _eIOOR;
+	            if (collection.listeners[i - 1] != null)
+	            {
+	                var l = collection.listeners[i - 1];
+                    l(t);
+					if (removeAfterInvoke){
+					    if (onceListeners.listeners[i - 1] == l){
+						    #if SIGTRAP_RELAY_DBG
+						        _RelayDebugger.DebugRemListener(this, onceListeners.listeners[i-1]);
+                            #endif
+					        onceListeners.RemoveAt(i-1);
+					    }
+					}
+	            }
+	            else {
+	                collection.RemoveAt(i - 1);
+	            }
+	        }
+        }
 	}
 	public class Relay<T,U> : RelayBase<Action<T,U>>, IRelayLink<T, U> {
 		private IRelayLink<T, U> _link = null;
@@ -346,29 +393,34 @@ namespace Sigtrap.Relays {
 				return _link;
 			}
 		}
-		
+        
 		public void Dispatch(T t, U u){
-			for (uint i=_count; i>0; --i){
-				if (i>_count) throw _eIOOR;
-				if (_listeners[i-1] != null){
-					_listeners[i-1](t, u);
-				} else {
-					RemoveAt(i-1);
-				}
-			}
-			for (uint i=_onceCount; i>0; --i){
-				if (_listenersOnce[i-1] != null){
-					var l = _listenersOnce[i-1];
-					l(t, u);
-					if (_listenersOnce[i-1] == l){
-						#if SIGTRAP_RELAY_DBG
-						_RelayDebugger.DebugRemListener(this, _listenersOnce[i-1]);
-						#endif
-						_onceCount = RemoveAt(_listenersOnce, _onceCount, i-1);
-					}
-				}
-			}
+		    IterateAndInvoke(persistantListeners, false, t, u);
+		    IterateAndInvoke(onceListeners, true, t, u);
 		}
+
+	    void IterateAndInvoke(RelayDelegateCollection collection, bool removeAfterInvoke, T t, U u){
+	        for (uint i = collection.count; i > 0; --i)
+	        {
+	            if (i > collection.count) throw _eIOOR;
+	            if (collection.listeners[i - 1] != null)
+	            {
+	                var l = collection.listeners[i - 1];
+                    l(t, u);
+					if (removeAfterInvoke){
+					    if (onceListeners.listeners[i - 1] == l){
+						    #if SIGTRAP_RELAY_DBG
+						        _RelayDebugger.DebugRemListener(this, onceListeners.listeners[i-1]);
+                            #endif
+					        onceListeners.RemoveAt(i-1);
+					    }
+					}
+	            }
+	            else {
+	                collection.RemoveAt(i - 1);
+	            }
+	        }
+        }
 	}
 	public class Relay<T,U,V> : RelayBase<Action<T,U,V>>, IRelayLink<T, U, V> {
 		private IRelayLink<T, U, V> _link = null;
@@ -385,29 +437,34 @@ namespace Sigtrap.Relays {
 				return _link;
 			}
 		}
-
+        
 		public void Dispatch(T t, U u, V v){
-			for (uint i=_count; i>0; --i){
-				if (i>_count) throw _eIOOR;
-				if (_listeners[i-1] != null){
-					_listeners[i-1](t, u, v);
-				} else {
-					RemoveAt(i-1);
-				}
-			}
-			for (uint i=_onceCount; i>0; --i){
-				if (_listenersOnce[i-1] != null){
-					var l = _listenersOnce[i-1];
-					l(t, u, v);
-					if (_listenersOnce[i-1] == l){
-						#if SIGTRAP_RELAY_DBG
-						_RelayDebugger.DebugRemListener(this, _listenersOnce[i-1]);
-						#endif
-						_onceCount = RemoveAt(_listenersOnce, _onceCount, i-1);
-					}
-				}
-			}
+		    IterateAndInvoke(persistantListeners, false, t, u, v);
+		    IterateAndInvoke(onceListeners, true, t, u, v);
 		}
+
+	    void IterateAndInvoke(RelayDelegateCollection collection, bool removeAfterInvoke, T t, U u, V v){
+	        for (uint i = collection.count; i > 0; --i)
+	        {
+	            if (i > collection.count) throw _eIOOR;
+	            if (collection.listeners[i - 1] != null)
+	            {
+	                var l = collection.listeners[i - 1];
+                    l(t, u, v);
+					if (removeAfterInvoke){
+					    if (onceListeners.listeners[i - 1] == l){
+						    #if SIGTRAP_RELAY_DBG
+						        _RelayDebugger.DebugRemListener(this, onceListeners.listeners[i-1]);
+                            #endif
+					        onceListeners.RemoveAt(i-1);
+					    }
+					}
+	            }
+	            else {
+	                collection.RemoveAt(i - 1);
+	            }
+	        }
+        }
 	}
 	public class Relay<T,U,V,W> : RelayBase<Action<T,U,V,W>>, IRelayLink<T, U, V, W> {
 		private IRelayLink<T, U, V, W> _link = null;
@@ -424,29 +481,34 @@ namespace Sigtrap.Relays {
 				return _link;
 			}
 		}
-
+        
 		public void Dispatch(T t, U u, V v, W w){
-			for (uint i=_count; i>0; --i){
-				if (i>_count) throw _eIOOR;
-				if (_listeners[i-1] != null){
-					_listeners[i-1](t, u, v, w);
-				} else {
-					RemoveAt(i-1);
-				}
-			}
-			for (uint i=_onceCount; i>0; --i){
-				if (_listenersOnce[i-1] != null){
-					var l = _listenersOnce[i-1];
-					l(t, u, v, w);
-					if (_listenersOnce[i-1] == l){
-						#if SIGTRAP_RELAY_DBG
-						_RelayDebugger.DebugRemListener(this, _listenersOnce[i-1]);
-						#endif
-						_onceCount = RemoveAt(_listenersOnce, _onceCount, i-1);
-					}
-				}
-			}
+		    IterateAndInvoke(persistantListeners, false, t, u, v, w);
+		    IterateAndInvoke(onceListeners, true, t, u, v, w);
 		}
+
+	    void IterateAndInvoke(RelayDelegateCollection collection, bool removeAfterInvoke, T t, U u, V v, W w){
+	        for (uint i = collection.count; i > 0; --i)
+	        {
+	            if (i > collection.count) throw _eIOOR;
+	            if (collection.listeners[i - 1] != null)
+	            {
+	                var l = collection.listeners[i - 1];
+                    l(t, u, v, w);
+					if (removeAfterInvoke){
+					    if (onceListeners.listeners[i - 1] == l){
+						    #if SIGTRAP_RELAY_DBG
+						        _RelayDebugger.DebugRemListener(this, onceListeners.listeners[i-1]);
+                            #endif
+					        onceListeners.RemoveAt(i-1);
+					    }
+					}
+	            }
+	            else {
+	                collection.RemoveAt(i - 1);
+	            }
+	        }
+        }
 	}
 	#endregion
 }
